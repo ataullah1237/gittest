5fcaaa1c557d9871d1ce3910bd703b3f617a4b3c
saQif's home <saQif's home@10.67.231.190>
2022-11-22 20:24:36 -0500

    first git project

---
+++ src/java_practice/Methods.java
@@ -0,0 +1,11 @@
+package java_practice;
+
+public class Methods {
+
+	public String ValidateHeader()
+	{
+		System.out.println("Header links validated");
+		return "Pass"; // if u want to return a string u have to change the "void" to "string"
+	}
+
+}
---
+++ src/java_practice/Parent.java
@@ -0,0 +1,24 @@
+package java_practice;
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//add 2 numbers 2,3  5
+		
+		int a=2;
+		int b=3;
+		int sum=a+b;
+				System.out.println(sum);
+				System.out.println("Sum is" +  sum); // sum is 5
+				
+				//3 lines of code header
+				
+				System.out.println("I Navigated to home page");
+				Methods m=new Methods();
+				System.out.println(m.ValidateHeader()); //Classobject.methodname
+//ClassB cb= new ClassB();
+
+	}
+
+}
